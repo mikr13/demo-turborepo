f7eb7c51d79e55fcc22cf0440ef77b9d997278c5
Mihir Kumar <mkumar@twilio.com>
2022-10-13 16:45:03 +0530

    feat: :sparkles: Button with props

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Button } from "ui";
+import { Button, ButtonWithProps } from "ui";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      <hr />
+      <ButtonWithProps text="Beep" />
     </div>
   );
 }
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -2,3 +2,7 @@ import * as React from "react";
 export const Button = () => {
   return <button>Boop</button>;
 };
+
+export const ButtonWithProps = (props: { text: string }) => {
+  return <button>{props.text}</button>;
+};
